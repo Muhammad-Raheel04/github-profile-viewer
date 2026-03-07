0b55c50d17abf64195db94f820b9c64ee912a6c1
Muhammad Raheel <raheel31102004@gmail.com>
2026-03-07 23:13:38 +0500

    integrated Header and Searchbar into App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
+import Header from './components/Header'
+import Searchbar from './components/Searchbar'
 
 function App() {
  
 
   return (
     <>
-     
+     <Header></Header>
+     <Searchbar/>
     </>
   )
 }
