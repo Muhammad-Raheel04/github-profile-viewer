ddce7e6853c8200ff4267971c04f85246660ae04
Muhammad Raheel <raheel31102004@gmail.com>
2026-03-08 09:17:43 +0500

    improved UI and integrated props in search bar component for username state

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import { Search } from 'lucide-react'
-const Searchbar = () => {
+const Searchbar = ({
+    userName,
+    setUsername
+}) => {
     return (
-        <div className='flex justify-center mt-4'>
-            <div className='flex items-center max-w-80 outline-none border border-black rounded-md px-4'>
+        <div className='flex justify-center mt-4 '>
+            <div className='flex items-center outline-none bg-white shadow w-[90vw] rounded-md px-4'>
                 <Search 
                 className='w-4 h-4 text-gray-500'
                 />
                 <input
                     type='text'
                     placeholder='Search GitHub username...'
-                    className='p-2 outline-none'
+                    className='p-2 outline-none w-full'
+                    value={userName}
+                    onChange={(e)=>setUsername(e.target.value)}
                 ></input>
             </div>
         </div>
