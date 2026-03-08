998b3f35830df73eae829576a1d80f2e780bab07
Muhammad Raheel <raheel31102004@gmail.com>
2026-03-08 09:19:18 +0500

    added hover affect to header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import { Github } from 'lucide-react'
 const Header = () => {
     return (
-        <div className='flex items-center w-full bg-black text-white text-2xl p-4 gap-4'>
-            <Github className='w-5 h-5 '></Github>
-            <h1>Github Profile Viewer</h1>
+        <div className='flex items-center w-full bg-black text-white text-2xl p-4 gap-4 '>
+            <Github className='w-5 h-5 hover:text-gray-400 transition '></Github>
+            <h1 className='hover:text-gray-400 transition'>Github Profile Viewer</h1>
         </div>
     )
 }
