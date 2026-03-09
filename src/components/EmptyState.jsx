a74394dd1975b2f7165e73c46d6a87b527a7b828
Muhammad Raheel <raheel31102004@gmail.com>
2026-03-09 13:22:39 +0500

    center aligned text of h1 and p tag for Better UI

--- src/components/EmptyState.jsx
+++ src/components/EmptyState.jsx
@@ -5,9 +5,9 @@ const EmptyState = () => {
   return (
     <div className='flex flex-col justify-center items-center mt-20 text-gray gap-3'>
       <Github className='w-20 h-20 text-gray-300 font-normal mb-4'></Github>
-      <h1 className='text-2xl font-semibold'>Search for a GitHub Profile</h1>
+      <h1 className='text-2xl font-semibold text-center'>Search for a GitHub Profile</h1>
       <p className='text-gray-400 text-center'>Enter a username to view their profile and repositories</p>
-      <p>Try: <span className='bg-gray-200 rounded-md p-1'>Muhammad-Raheel04</span></p>
+      <p>Try: <span className='bg-gray-200 rounded-md p-1 text-center'>Muhammad-Raheel04</span></p>
     </div>
   )
 }
