82c44c2702ba7abdc1d8bda484f2b6e235bfc9b1
Muhammad Raheel <raheel31102004@gmail.com>
2026-03-09 13:25:31 +0500

    added search bar to take username from user

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Search } from 'lucide-react'
 const Searchbar = ({
-    userName,
+    username,
     setUsername
 }) => {
     return (
-        <div className='flex justify-center mt-4 '>
+        <div className='flex justify-center my-4 '>
             <div className='flex items-center outline-none bg-white shadow w-[90vw] rounded-md px-4'>
                 <Search 
                 className='w-4 h-4 text-gray-500'
@@ -14,7 +14,7 @@ const Searchbar = ({
                     type='text'
                     placeholder='Search GitHub username...'
                     className='p-2 outline-none w-full'
-                    value={userName}
+                    value={username}
                     onChange={(e)=>setUsername(e.target.value)}
                 ></input>
             </div>
