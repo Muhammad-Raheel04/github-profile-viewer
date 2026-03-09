391b8b41ff6abd0806a18c3ba6511f164e38624a
Muhammad Raheel <raheel31102004@gmail.com>
2026-03-09 12:25:50 +0500

    integrated empty state, profile card and repo card components and configured api to fetch user github proile and repo data

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,43 @@
 import './App.css'
+import EmptyState from './components/EmptyState'
 import Header from './components/Header'
+import ProfileCard from './components/ProfileCard'
 import Searchbar from './components/Searchbar'
-
+import RepoCard from './components/RepoCard'
+import { useEffect, useState } from 'react'
 function App() {
- 
+  const [username, setUsername] = useState("");
+  const [userRepoData, setUserRepoData] = useState(null)
+  const [data, setUserData] = useState(null)
+  useEffect(() => {
+    if (!username) return;
+    const timer = setTimeout(async() => {
+      try{
+        const userRes=await  fetch(`https://api.github.com/users/${username}`);
+        const userData=await userRes.json();
+        setUserData(userData);
 
+        const reposRes=await fetch(`https://api.github.com/users/${username}/repos`);
+        const userRepoData=await reposRes.json();
+        setUserRepoData(userRepoData);
+
+      }catch(error){
+        console.log(error);
+      }
+    }, 500)
+    return () => clearTimeout(timer);
+
+  }, [username])
   return (
     <>
-     <Header></Header>
-     <Searchbar/>
+      <Header></Header>
+      <Searchbar username={username} setUsername={setUsername} />
+      {
+        data ? <ProfileCard userData={data} /> : <EmptyState />
+      }
+      {
+        username && userRepoData ? <RepoCard userRepoData={userRepoData}></RepoCard> : ''
+      }
     </>
   )
 }
